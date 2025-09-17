de3814b433f0541b88bd383a37f9eff1aedeb4b2
JYParker <junsparker@gmail.com>
2025-09-17 13:07:24 +0900

    로그인 기능 함수 추가

--- util/account.py
+++ util/account.py
@@ -1,13 +1,18 @@
 from pymongo import MongoClient
 import bcrypt
+import models.user_model
 
+
+#비밀번호 해싱 후 저장
 def hashing(pw):
     hashed = bcrypt.hashpw(pw.encode(), bcrypt.gensalt()).decode()
     return hashed
 
+# 로그인 시 비밀번호 체크
 def pw_check(pw, hashed):
     return bcrypt.checkpw(pw.encode(), hashed.encode())
 
+# 아이디가 이미 있는 아이디인지 체크
 def check_double_regist(id):
     client = MongoClient('mongodb://localhost:27017/')
     db = client['user_db']
@@ -17,8 +22,9 @@ def check_double_regist(id):
         return False  # 컬렉션이 비어있으면 무조건 가입 가능
     else:
         user = collection.find_one({'id':id})
-        return user is not None    
+        return user is not None
 
+#로그인 함수
 def sign_in(info_list):
     client = MongoClient('mongodb://localhost:27017/')
     db = client['user_db']
@@ -31,11 +37,10 @@ def sign_in(info_list):
 
     if pw_check(id, user['pw']) and user:
         return id
-    
     else:
         False
 
-
+#회원가입함수
 def sign_up(info_list):
     client = MongoClient('mongodb://localhost:27017/')
     db = client['user_db']
@@ -48,7 +53,7 @@ def sign_up(info_list):
     "pw" : hashing(info_list[3])
     }
 
-    double_check = check_double_regist(info_list[2])
+    double_check = check_double_regist(info_list[2])    #이미 존재하는 아이디인지 체크
     if double_check:
         return [False, "이미 존재하는 아이디입니다."]
     else:
@@ -56,9 +61,4 @@ def sign_up(info_list):
             collection.insert_one(user)
             return True
         except:
-            return [False,"오류가 발생하였습니다. 다시 시도해주세요"]
-
-
-
-
-
+            return [False,"오류가 발생하였습니다. 다시 시도해주세요"]  # 오류 발생 체크
